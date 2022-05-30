0ffe0872229d58367bbc84ea5d3a734841663428
yagazierem <adaogbonna44@gmail.com>
2022-05-30 12:08:07 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function lyold() {
+    let fNum = prompt("Enter a number");
+    let sNum = prompt("Enter another number");
+    let product = Number(fNum) * Number(sNum)
+    alert(product)
+}  
+lyold()
